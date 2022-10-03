4645d6291d4bb194c8c66dd2f8cde3cf5b8d49a2
mishakurylev <kvepo@mail.ru>
2022-10-03 23:28:25 +0300

    make log form

--- src/app/index.php
+++ src/app/index.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Авторизация</title>
-    <link rel="stylesheet" href="../templates/css/style.css"
+    <link rel="stylesheet" href="/app/css/style.css"
 </head>
 <body>
     <form action="" method="">
         <label>Логин</label>
-        <input type="text">
+        <input type="text" placeholder="Введите логин">
         <label>Пароль</label>
-        <input type="text">
+        <input type="text" placeholder="Введите пароль">
+        <button>Войти в аккаунт</button>
+        <p>
+            Нет аккаунта? - <a href="#">можете зарегестрироваться тут</a>
+        </p>
     </form>
 </body>
 </html>
---
+++ src/app/reg.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="/app/css/style.css"
+</head>
+<body>
+<form  class="form_auth" action="" method="">
+    <label>Фамилия и имя</label>
+    <input type="text" placeholder="Введите фамилию и имя">
+    <label>Логин</label>
+    <input type="text" placeholder="Введите логин">
+    <label>Пароль</label>
+    <input type="text" placeholder="Введите пароль">
+    <label>Email</label>
+    <input type="text" placeholder="Введите Email">
+    <button>Зарегистрироваться</button>
+    <p>
+        Уже есть аккаунт? - <a href="#">авторизируйтесь тут</a>
+    </p>
+</form>
+</body>
+</html>
