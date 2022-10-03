3875497777dec7397bfab5df24005b59c81e52ff
Misha <mihail.kurylev09@mail.ru>
2022-10-03 12:38:58 +0300

    new_init

---
+++ src/app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="../templates/css/style.css"
+</head>
+<body>
+    <form action="" method="">
+        <label>Логин</label>
+        <input type="text">
+        <label>Пароль</label>
+        <input type="text">
+    </form>
+</body>
+</html>
